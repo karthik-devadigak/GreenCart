223abfbc7afa716c0388aa248fb9725e9a5da42a
Karthik <kdevadiga91@gmail.com>
2026-02-04 10:06:28 +0530

    intial

---
+++ src/test/java/com/test/GreenCart.java
@@ -0,0 +1,36 @@
+package com.test;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.time.chrono.ChronoLocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class GreenCart {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        driver.get("https://rahulshettyacademy.com/seleniumPractise");
+        By itemNameLocator= By.xpath("//h4[@class='product-name']");
+        List<WebElement> itemNames=  wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(itemNameLocator));
+        String[] requiredItems = {"Brocolli", "Carrot", "Capsicum"};
+        List<String> requiredItemList = new ArrayList<>(Arrays.asList(requiredItems));
+
+        for(WebElement itemNamesRaw : itemNames){
+            String itemName = itemNamesRaw.getText().split("-")[0].trim();
+
+            if(requiredItemList.contains(itemName)) {
+                itemNamesRaw.findElement(By.xpath("./../div[@class=\"product-action\"]/button")).click();
+            }
+        }
+
+
+    }
+}
